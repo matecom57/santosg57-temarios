31456370b5c61185e0007282c6cd36216e8b99f2
santosg572 <santosg572@gmail.com>
2025-11-25 09:05:38 -0600

    Test comité

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Cursos'
+copyright = '2025, santosg57'
+author = 'santosg57'
 
 release = '0.1'
 version = '0.1.0'
